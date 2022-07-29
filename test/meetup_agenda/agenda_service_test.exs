719365d9ce6b3f984e9c48af26c200ba880ee942
Caleb Acosta <caleb.acosta.j@gmail.com>
2022-07-29 12:12:44 -0500

    Tests

--- test/meetup_agenda/agenda_service_test.exs
+++ test/meetup_agenda/agenda_service_test.exs
@@ -10,9 +10,26 @@ defmodule MeetupAgenda.AgendaServiceTest do
 
     @invalid_attrs %{description: nil, event_date: nil, title: nil}
 
+    @valid_attrs %{
+      description: "some description",
+      event_date: ~D[2022-07-10],
+      title: "some title"
+    }
     test "list_events/0 returns all events" do
       event = event_fixture()
       assert AgendaService.list_events() == [event]
+    end
+
+    test "list_events_by/3 returns all events with the given month" do
+      event = event_fixture()
+
+      assert AgendaService.list_events_by(:month, event.event_date.year, event.event_date.month) ==
+               [event]
+    end
+
+    test "list_events_by/3 returns all events on the given date" do
+      event = event_fixture()
+      assert AgendaService.list_events_by(:date, event.event_date) == [event]
     end
 
     test "get_event!/1 returns the event with given id" do
@@ -21,13 +38,7 @@ defmodule MeetupAgenda.AgendaServiceTest do
     end
 
     test "create_event/1 with valid data creates a event" do
-      valid_attrs = %{
-        description: "some description",
-        event_date: ~D[2022-07-10],
-        title: "some title"
-      }
-
-      assert {:ok, %Event{} = event} = AgendaService.create_event(valid_attrs)
+      assert {:ok, %Event{} = event} = AgendaService.create_event(@valid_attrs)
       assert event.description == "some description"
       assert event.event_date == ~D[2022-07-10]
       assert event.title == "some title"
@@ -35,6 +46,12 @@ defmodule MeetupAgenda.AgendaServiceTest do
 
     test "create_event/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = AgendaService.create_event(@invalid_attrs)
+    end
+
+    test "create_event_strict/1 with repeated date returns error changeset" do
+      {:ok, _event} = AgendaService.create_event_strict(@valid_attrs)
+      {:error, changeset} = AgendaService.create_event_strict(@valid_attrs)
+      assert !changeset.valid?
     end
 
     test "update_event/2 with valid data updates the event" do
@@ -56,6 +73,19 @@ defmodule MeetupAgenda.AgendaServiceTest do
       event = event_fixture()
       assert {:error, %Ecto.Changeset{}} = AgendaService.update_event(event, @invalid_attrs)
       assert event == AgendaService.get_event!(event.id)
+    end
+
+    test "update_event_strict/2 with repeated date returns error changeset" do
+      {:ok, event1} =
+        AgendaService.create_event(%{title: "t1", description: "d1", event_date: ~D[2022-07-11]})
+
+      {:ok, event2} =
+        AgendaService.create_event(%{title: "t2", description: "d2", event_date: ~D[2022-07-12]})
+
+      {:error, changeset} =
+        AgendaService.update_event_strict(event2, %{event_date: event1.event_date})
+
+      assert !changeset.valid?
     end
 
     test "delete_event/1 deletes the event" do
--- test/meetup_agenda_web/live/event_live_test.exs
+++ test/meetup_agenda_web/live/event_live_test.exs
@@ -5,16 +5,29 @@ defmodule MeetupAgendaWeb.EventLiveTest do
   import MeetupAgenda.AgendaServiceFixtures
 
   @create_attrs %{
-    description: "some description",
-    event_date: %{day: 10, month: 7, year: 2022},
-    title: "some title"
+    title: "event title",
+    description: "event description",
+    ordinal_day: "1",
+    day: "1",
+    month: "1",
+    year: Timex.today().year |> Integer.to_string()
   }
   @update_attrs %{
-    description: "some updated description",
-    event_date: %{day: 11, month: 7, year: 2022},
-    title: "some updated title"
+    description: "event updated description",
+    title: "some updated title",
+    ordinal_day: "2",
+    day: "2",
+    month: "2",
+    year: Timex.today().year |> Integer.to_string()
   }
-  @invalid_attrs %{description: nil, event_date: %{day: 30, month: 2, year: 2022}, title: nil}
+  @invalid_attrs %{
+    description: nil,
+    title: nil,
+    ordinal_day: "1",
+    day: "1",
+    month: "1",
+    year: Timex.today().year |> Integer.to_string()
+  }
 
   defp create_event(_) do
     event = event_fixture()
@@ -28,57 +41,51 @@ defmodule MeetupAgendaWeb.EventLiveTest do
       {:ok, _index_live, html} = live(conn, Routes.event_index_path(conn, :index))
 
       assert html =~ "Listing Events"
-      assert html =~ event.description
+      assert html =~ event.title
     end
 
     test "saves new event", %{conn: conn} do
-      {:ok, index_live, _html} = live(conn, Routes.event_index_path(conn, :index))
+      {:ok, index_live, html} = live(conn, Routes.event_index_path(conn, :index))
+
+      refute html =~ "modal is-active"
 
       assert index_live |> element("a", "New Event") |> render_click() =~
-               "New Event"
+               "modal is-active"
 
       assert_patch(index_live, Routes.event_index_path(conn, :new))
 
       assert index_live
-             |> form("#event-form", event: @invalid_attrs)
-             |> render_change() =~ "is invalid"
+             |> form("[data-phx-component] form", event: @invalid_attrs)
+             |> render_submit =~ "can&#39;t be blank"
 
-      {:ok, _, html} =
-        index_live
-        |> form("#event-form", event: @create_attrs)
-        |> render_submit()
-        |> follow_redirect(conn, Routes.event_index_path(conn, :index))
+      index_live
+      |> form("[data-phx-component] form", event: @create_attrs)
+      |> render_submit()
 
-      assert html =~ "Event created successfully"
-      assert html =~ "some description"
+      assert render(index_live) =~ "Event created successfully"
     end
 
     test "updates event in listing", %{conn: conn, event: event} do
       {:ok, index_live, _html} = live(conn, Routes.event_index_path(conn, :index))
 
-      assert index_live |> element("#event-#{event.id} a", "Edit") |> render_click() =~
-               "Edit Event"
+      assert index_live
+             |> element("[href=\"\/events\/#{event.id}\/edit\"]", "Edit")
+             |> render_click() =~
+               "Edit event"
 
       assert_patch(index_live, Routes.event_index_path(conn, :edit, event))
 
-      assert index_live
-             |> form("#event-form", event: @invalid_attrs)
-             |> render_change() =~ "is invalid"
+      index_live
+      |> form("[data-phx-component] form", event: @update_attrs)
+      |> render_submit()
 
-      {:ok, _, html} =
-        index_live
-        |> form("#event-form", event: @update_attrs)
-        |> render_submit()
-        |> follow_redirect(conn, Routes.event_index_path(conn, :index))
-
-      assert html =~ "Event updated successfully"
-      assert html =~ "some updated description"
+      assert render(index_live) =~ "Event updated successfully"
     end
 
     test "deletes event in listing", %{conn: conn, event: event} do
       {:ok, index_live, _html} = live(conn, Routes.event_index_path(conn, :index))
 
-      assert index_live |> element("#event-#{event.id} a", "Delete") |> render_click()
+      assert index_live |> element("[phx-value-id=#{event.id}]", "Delete") |> render_click()
       refute has_element?(index_live, "#event-#{event.id}")
     end
   end
@@ -87,32 +94,122 @@ defmodule MeetupAgendaWeb.EventLiveTest do
     setup [:create_event]
 
     test "displays event", %{conn: conn, event: event} do
-      {:ok, _show_live, html} = live(conn, Routes.event_show_path(conn, :show, event))
+      {:ok, _show_live, html} = live(conn, Routes.event_index_path(conn, :show, event))
 
       assert html =~ "Show Event"
       assert html =~ event.description
     end
 
     test "updates event within modal", %{conn: conn, event: event} do
-      {:ok, show_live, _html} = live(conn, Routes.event_show_path(conn, :show, event))
+      {:ok, show_live, html} = live(conn, Routes.event_index_path(conn, :show, event))
 
-      assert show_live |> element("a", "Edit") |> render_click() =~
-               "Edit Event"
-
-      assert_patch(show_live, Routes.event_show_path(conn, :edit, event))
+      assert html =~ event.description
+      element(show_live, ".is-primary[href]", "Edit") |> render_click()
+      assert_patch(show_live, Routes.event_index_path(conn, :edit, event))
 
       assert show_live
-             |> form("#event-form", event: @invalid_attrs)
-             |> render_change() =~ "is invalid"
+             |> form("[data-phx-component] form", event: @invalid_attrs)
+             |> render_submit() =~ "can&#39;t be blank"
 
-      {:ok, _, html} =
-        show_live
-        |> form("#event-form", event: @update_attrs)
-        |> render_submit()
-        |> follow_redirect(conn, Routes.event_show_path(conn, :show, event))
+      show_live
+      |> form("[data-phx-component] form", event: @update_attrs)
+      |> render_submit()
 
-      assert html =~ "Event updated successfully"
-      assert html =~ "some updated description"
+      assert render(show_live) =~ "Event updated successfully"
+    end
+  end
+
+  describe "Modal" do
+    test "closes when click close button", %{conn: conn} do
+      {:ok, new_live, html} = live(conn, Routes.event_index_path(conn, :new))
+      assert html =~ "class=\"modal is-active\""
+
+      refute new_live |> element("[phx-click=modal_close]") |> render_click() =~
+               "class=\"modal is-active\""
+    end
+  end
+
+  describe "Panel" do
+    test "switches between tabs", %{conn: conn} do
+      {:ok, index_live, html} = live(conn, Routes.event_index_path(conn, :index))
+      assert html =~ "<a phx-click=\"switch_tab\" class=\"is-active\" phx-value-index=\"0\">"
+      index_live |> element("[phx-value-index=\"1\"]") |> render_click()
+
+      render(index_live) =~
+        "<a class=\"is-active\" phx-click=\"switch_tab\" phx-value-index=\"1\">"
+    end
+
+    test "change filter month", %{conn: conn} do
+      today = Timex.today()
+
+      {:ok, index_live, html} = live(conn, Routes.event_index_path(conn, :index))
+
+      assert html =~ Timex.month_shortname(today.month)
+
+      # Forward
+      assert index_live |> element("[phx-click=\"next_month\"]") |> render_click() =~
+               Timex.month_shortname(Timex.shift(today, months: 1).month)
+
+      {:ok, index_live, _html} = live(conn, Routes.event_index_path(conn, :index))
+
+      # Backward
+      assert index_live |> element("[phx-click=\"prev_month\"]") |> render_click() =~
+               Timex.month_shortname(Timex.shift(today, months: -1).month)
+    end
+  end
+
+  describe "Strict mode" do
+    setup [:create_event]
+
+    test "doesn't insert duplicated dates", %{conn: conn, event: event} do
+      duplicated_date_event = %{
+        title: "event title",
+        description: "description",
+        ordinal_day: MeetupAgendaWeb.EventLive.FormComponent.ordinal_weekday(event.event_date),
+        day: Timex.weekday(event.event_date),
+        month: event.event_date.month,
+        year: event.event_date.year
+      }
+
+      {:ok, index_live, _html} = live(conn, Routes.event_index_path(conn, :index))
+      index_live |> element("#strict") |> render_click()
+
+      index_live |> element("a", "New Event") |> render_click()
+
+      index_live
+      |> form("[data-phx-component] form", event: duplicated_date_event)
+      |> render_submit()
+
+      assert render(index_live) =~ "has already been taken"
+    end
+
+    test "doesn't update duplicated dates", %{conn: conn, event: event} do
+      {:ok, new_event} =
+        MeetupAgenda.AgendaService.create_event(
+          Map.update!(event, :event_date, &Timex.shift(&1, days: 1))
+          |> Map.from_struct()
+        )
+
+      duplicated_date_event = %{
+        title: new_event.title,
+        description: new_event.description,
+        ordinal_day: MeetupAgendaWeb.EventLive.FormComponent.ordinal_weekday(event.event_date),
+        day: Timex.weekday(event.event_date),
+        month: event.event_date.month,
+        year: event.event_date.year
+      }
+
+      {:ok, edit_live, _html} = live(conn, Routes.event_index_path(conn, :index))
+
+      edit_live |> element("#strict") |> render_click()
+
+      render_patch(edit_live, Routes.event_index_path(conn, :edit, new_event))
+
+      edit_live
+      |> form("[data-phx-component] form", event: duplicated_date_event)
+      |> render_submit()
+
+      assert render(edit_live) =~ "has already been taken"
     end
   end
 end
